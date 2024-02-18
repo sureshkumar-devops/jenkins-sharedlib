8338a67271ba7ec4d7e4ede94f1cad898e6d5222
sureshkumar-devops <suresh.ak47@gmail.com>
2024-02-18 10:55:59 +0530

    added new feature

--- vars/scan.groovy
+++ vars/scan.groovy
@@ -1,3 +1,13 @@
 def call(){
-  echo "Executing the Scaning method....."
+  echo "Executing the Scaning method....."  
+  def all = [ Jenkins.instance ]
+all.addAll(Jenkins.instance.nodes)
+all.each {
+    def c = it.toComputer()
+    println "[$it.nodeName] - $it.numExecutors - $it.assignedLabels - $it.acceptingTasks - $it.nodeProperties - $c.offline"
+}
+println "----"
+Jenkins.instance.queue.items.each {
+    println "$it.id $it.blocked $it.buildable $it.stuck $it.assignedLabel $it.causes"
+}
 }
