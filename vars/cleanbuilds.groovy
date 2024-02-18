90e3f440442b179711fbf5d6299878d818be3496
sureshkumar-devops <suresh.ak47@gmail.com>
2024-02-18 10:32:58 +0530

    added to discard old builds

--- vars/cleanbuilds.groovy
+++ vars/cleanbuilds.groovy
@@ -1,6 +1,32 @@
 
 def callBuildDiscarder(){
-  echo "Discard Old Builds..."
-  options {
-  buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '2')
+  //echo "Discard Old Builds..."
+  //options {
+  //buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '2')
+
+
+def daysToKeep = ''
+def numToKeep = 2
+def artifactDaysToKeepStr = ''
+def artifactNumToKeepStr = 2
+
+Jenkins.instance.items.each { item ->
+  println("=====================")
+  println("JOB: " + item.name)
+  println("Job type: " + item.getClass())
+
+  if(item.buildDiscarder == null) {
+    println("No BuildDiscarder")
+    println("Set BuildDiscarder to LogRotator")
+  } else {
+    println("BuildDiscarder: " + item.buildDiscarder.getClass())
+    println("Found setting: " + "days to keep=" + item.buildDiscarder.daysToKeepStr + "; num to keep=" + item.buildDiscarder.numToKeepStr + "; artifact day to keep=" + item.buildDiscarder.artifactDaysToKeepStr + "; artifact num to keep=" + item.buildDiscarder.artifactNumToKeepStr)
+    println("Set new setting")
+  }
+
+  item.buildDiscarder = new hudson.tasks.LogRotator(daysToKeep,numToKeep, artifactDaysToKeep, artifactNumToKeep)
+  item.save()
+  println("")
+
+}
 }
