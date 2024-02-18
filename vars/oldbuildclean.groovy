3c8ea42cd5a2f6d3dd2fa5c02e84eed3227000b2
sureshkumar-devops <suresh.ak47@gmail.com>
2024-02-18 11:29:34 +0530

    updated

--- vars/oldbuildclean.groovy
+++ vars/oldbuildclean.groovy
@@ -1,8 +1,4 @@
-def cleanoldbuilds1() {
-    properties([
-buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '2')
-  ])
-}
+
 
  def cleanoldbuilds() {
   properties([
