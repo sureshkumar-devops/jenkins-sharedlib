5e779ec0802869a35a9f9f4e277d5165f30f064b
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-04-01 14:36:48 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,7 +1,5 @@
 def call(String projectName,String imageTag, String dockerHubUser, String dockerfilepath, String sourcepath)
 {
-   
-   echo "docker build -t ${dockerHubUser}/${projectName}:${imageTag} -f ${dockerfilepath} ${sourcepath}"  
-   
+   sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} -f ${dockerfilepath} ${sourcepath}"  
 }
 
