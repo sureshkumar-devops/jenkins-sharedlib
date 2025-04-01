a6b540129b6e32ea3694be1917b442da628eed32
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-04-01 15:13:30 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -3,3 +3,15 @@ def build(String projectName,String imageTag, String dockerHubUser, String docke
    echo "docker build -t ${dockerHubUser}/${projectName}:${imageTag} -f ${dockerfilepath} ${sourcepath}"  
 }
 
+def push(String projectName,String imageTag, String dockerHubUser, String dockerfilepath, String sourcepath)
+{
+   withCredentials([usernamePassword(credentialsId: 'docker-credentials', 
+                                      usernameVariable: 'dockerHubUser', 
+                                      passwordVariable: 'dockerHubPass')]) 
+   {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
+   }
+}
+
+  
